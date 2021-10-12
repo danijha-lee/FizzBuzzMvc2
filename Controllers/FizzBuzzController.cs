ffe92b2483dc70465678916560ccde7361d0cb9d
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-12 15:07:33 -0400

    Add project files.

---
+++ Controllers/FizzBuzzController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzBuzzMvc2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzzMvc2.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new FizzBuzz();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(FizzBuzz model)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % model.FizzValue == 0 && i % model.BuzzValue == 0)
+                {
+                    model.Results.Add("FizzBuzz");
+                }
+                else if (i % model.FizzValue == 0)
+                {
+                    model.Results.Add("Fizz");
+                }
+                else if (i % model.BuzzValue == 0)
+                {
+                    model.Results.Add("Buzz");
+                }
+                else
+                {
+                    model.Results.Add(i.ToString());
+                }
+            }
+            var test = model.Results.Count;
+            return View(model);
+        }
+    }
+}
